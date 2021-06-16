e498f9eaba406cbbbe80b234711c40d326ca1eec
Zafar <cc.rafaz@zafar.cc>
2021-06-16 11:54:43 -0700

    [sparsity] Base sparsity level scheduler class
    ghstack-source-id: ef046762050870225d65077854aad7807e2dd30f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59770

--- test/test_ao_sparsity.py
+++ test/test_ao_sparsity.py
@@ -10,5 +10,8 @@ from ao.sparsity.test_kernels import TestQuantizedSparseLayers  # noqa: F401
 from ao.sparsity.test_sparsifier import TestBaseSparsifier  # noqa: F401
 from ao.sparsity.test_sparsifier import TestWeightNormSparsifier  # noqa: F401
 
+# Scheduler
+from ao.sparsity.test_scheduler import TestBaseScheduler  # noqa: F401
+
 if __name__ == '__main__':
     run_tests()
